d88f9bdc1f1bf738e3df66a120128af1210c4e32
rushikeshdoke580-pixel <rushikeshdoke580@gmail.com>
2026-03-21 11:20:11 +0530

    Create python_datatypes.py

---
+++ python_datatypes.py
@@ -0,0 +1,43 @@
+# Integer
+a = 18
+print(a)
+print(type(a))
+
+# Float
+marks = 76.40
+print(marks)
+print(type(marks))
+
+# String
+name = "Rushikesh Doke"
+print(name)
+print(type(name))
+
+# Boolean
+b = True
+print(b)
+print(type(b))
+
+# List
+x = [2, 4, 6, 8, 10]
+print(x)
+print(type(x))
+
+# Tuple
+numbers = (3, 6, 9, 12)
+print(numbers)
+print(type(numbers))
+
+# Dictionary
+xyz = {
+"Name": "Rushikesh Doke",
+"Age": 21,
+"Course": "Electrical Engineering"
+}
+print(xyz)
+print(type(xyz))
+
+# Set
+colors = {"Red", "Blue", "Green"}
+print(colors)
+print(type(colors))
